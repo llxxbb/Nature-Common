7e979fa6ddeb169a31c6ca40c30d55d3e88be025
llxxb <llxxbb@yeah.net>
2018-08-12 09:07:40 +0800

    rename common module name to error

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #[macro_use]
 extern crate serde_derive;
 
-pub use common::*;
+pub use error::*;
 pub use instance::*;
 pub use thing::*;
 pub use convertor::*;
 
 mod instance;
-mod common;
+mod error;
 mod thing;
 mod convertor;
