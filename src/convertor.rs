3425b773cf1d8ac7215a6f1e173a8233a19d97b2
llxxb <llxxbb@yeah.net>
2018-08-17 22:26:41 +0800

    add None field to ConverterReturned

--- src/convertor.rs
+++ src/convertor.rs
@@ -5,7 +5,11 @@ pub enum ConverterReturned {
     LogicalError(String),
     /// This can quick finish the process, and retry later.
     EnvError,
+    /// No instance would be return.
+    None,
+    /// Tell `Nature` the task will be processed asynchronously, and it will callback to `Nature` later will result are ready.
     Delay(u32),
+    /// Give result to `Nature`
     Instances(Vec<Instance>),
 }
 
