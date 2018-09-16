1a3dabb6a68add73d187694ea60a9570d568280b
llxxb <llxxbb@yeah.net>
2018-09-16 14:40:29 +0800

    add error transforms

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,6 @@
 use serde_json;
+use std;
+use uuid;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum NatureError {
@@ -28,6 +30,18 @@ pub enum NatureError {
 impl From<serde_json::error::Error> for NatureError {
     fn from(e: serde_json::error::Error) -> Self {
         NatureError::SerializeError(e.to_string())
+    }
+}
+
+impl From<uuid::ParseError> for NatureError {
+    fn from(_e: uuid::ParseError) -> Self {
+        NatureError::UuidParseError
+    }
+}
+
+impl From<std::num::ParseIntError> for NatureError {
+    fn from(_: std::num::ParseIntError) -> Self {
+        NatureError::UuidParseError
     }
 }
 
