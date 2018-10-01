e5382e1711578ef5c4f6b8e434c6fa3d57567ee2
llxxb <llxxbb@yeah.net>
2018-10-01 22:25:18 +0800

    add limit for content length

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 #![feature(int_to_from_bytes)] // this used to convert uuid to u128
 
-extern crate chrono;
-#[macro_use]
-extern crate log;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
