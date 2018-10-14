7d7f8f2f9670f7bc9e45b0c12c52d11273fa821d
llxxb <llxxbb@yeah.net>
2018-10-14 19:06:05 +0800

    implement delivery increase_times_and_delay function

--- src/util/id_tool.rs
+++ src/util/id_tool.rs
@@ -1,7 +1,9 @@
+extern crate itertools;
+
+use ::Result;
 use serde::Serialize;
 use serde_json;
 use uuid::*;
-use ::Result;
 
 #[inline]
 pub fn generate_id<T: ?Sized + Serialize>(value: &T) -> Result<u128> {
@@ -22,4 +24,23 @@ pub fn vec_to_u128(vec: &Vec<u8>) -> u128 {
         arr[i] = vec[i];
     }
     u128::from_ne_bytes(arr)
+}
+
+#[inline]
+pub fn vec_to_hex_string(vec: &Vec<u8>) -> String {
+    use self::itertools::Itertools;
+    vec.iter().format("", |e, f| f(&format_args!("{:02x}", e))).to_string()
+}
+
+#[cfg(test)]
+mod test {
+    use util::id_tool::vec_to_hex_string;
+
+    #[test]
+    fn vec_to_hex_string_test() {
+        let string = vec_to_hex_string(&vec!(1, 2, 3, 16));
+        assert_eq!(string, "01020310");
+        let string = vec_to_hex_string(&vec!(1, 2, 3, 15));
+        assert_eq!(string, "0102030f");
+    }
 }
