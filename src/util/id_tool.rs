9a507f39825e0e5032197c6ab206b7959fa2d054
llxxbb <llxxbb@yeah.net>
2018-12-09 10:30:40 +0800

    apply clippy

--- src/util/id_tool.rs
+++ src/util/id_tool.rs
@@ -1,9 +1,10 @@
 extern crate itertools;
 
-use ::Result;
 use serde::Serialize;
 use serde_json;
 use uuid::*;
+
+use ::Result;
 
 #[inline]
 pub fn generate_id<T: ?Sized + Serialize>(value: &T) -> Result<u128> {
@@ -18,16 +19,15 @@ pub fn u128_to_vec_u8(value: u128) -> Vec<u8> {
 }
 
 #[inline]
+#[allow(clippy::ptr_arg)]
 pub fn vec_to_u128(vec: &Vec<u8>) -> u128 {
     let mut arr = [0u8; 16];
-    for i in 0..16 {
-        arr[i] = vec[i];
-    }
+    arr[..16].clone_from_slice(&vec[..16]);
     u128::from_ne_bytes(arr)
 }
 
 #[inline]
-pub fn vec_to_hex_string(vec: &Vec<u8>) -> String {
+pub fn vec_to_hex_string(vec: &[u8]) -> String {
     use self::itertools::Itertools;
     vec.iter().format("", |e, f| f(&format_args!("{:02x}", e))).to_string()
 }
