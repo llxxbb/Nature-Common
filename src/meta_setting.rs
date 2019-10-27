e5983c235418ffbed7927af6cc3e419d5f11115e
llxxbb <llxxbb@yeah.net>
2019-10-27 19:43:00 +0800

    meta support belong_to

--- src/meta_setting.rs
+++ src/meta_setting.rs
@@ -7,6 +7,11 @@ pub struct MetaSetting {
     #[serde(skip_serializing_if = "is_false")]
     #[serde(default)]
     pub is_empty_content: bool,
+    /// A meta_string, this meta instance's id must equal protagonist instance's id.
+    /// only useful for state-meta.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    pub belong_to: Option<String>,
 }
 
 /// This is only used for serialize
@@ -24,6 +29,7 @@ mod test {
         let setting = MetaSetting {
             is_state: false,
             is_empty_content: true,
+            belong_to: None
         };
         let result = serde_json::to_string(&setting).unwrap();
         assert_eq!(result, r#"{"is_empty_content":true}"#)
